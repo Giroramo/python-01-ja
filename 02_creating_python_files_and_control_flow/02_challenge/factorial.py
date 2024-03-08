73972e009fb666ba01fe2dc2127900d53e317394
Jiro Fujino <jiro.fujino@woven-planet.global>
2024-03-08 14:57:19 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,11 +1,29 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
+    number = int(input('正の整数を入力して下さい'))
+    
+    if number == 0:
+        return 1
+    
+    # while loop
+    # count = number    
+    #        
+    #while( count > 0):
+    ##    if count ==1 :
+    #       break
+        
+    #    number  = number * (count-1)
+    #    count = count-1 """
+    # for loop
+    # range(5)      => 0, 1, 2, 3, 4
+    # range(1,5)    => 1, 2, 3, 4
+    # 1st step : oneNumber =1
+    for oneNumber in range(1, number):
+        number = number*oneNumber
 
     # result変数を編集し、最終的な計算結果を保存します
-    result = 0
-
+    result = number
     return result
 
 
