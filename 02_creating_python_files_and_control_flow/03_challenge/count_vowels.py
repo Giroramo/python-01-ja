5fdf3a73bcce02d6cf07c8f87c299fdd6ea3bdff
Jiro Fujino <jiro.fujino@woven-planet.global>
2024-03-11 23:09:02 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
+++ 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
@@ -1,1 +1,8 @@
 # ここにコードを書いてください
+usrinput = input("好きな英文を入力してください：")
+vowels = "aiueo"
+x = 0
+for char in usrinput.lower():
+    if char in vowels:
+        x += 1
+print("Number of vowels:"+ str(x))
