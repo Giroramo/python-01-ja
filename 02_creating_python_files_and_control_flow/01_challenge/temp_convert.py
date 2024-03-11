c65d8e99ba418567fd2c2e5dbefed39b83c828fe
Jiro Fujino <jiro.fujino@woven-planet.global>
2024-03-11 22:47:17 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,9 +1,12 @@
 def convert():
-    # ここにコードを書いてください
-    # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp = float(input("温度を入力してください: "))
+    unit = input("温度の単位を入力してください（摂氏の場合はc、華氏の場合はf）: ")
+
+    if unit.lower() == 'c':
+        temp = (temp * 9/5) + 32
+    elif unit.lower() == 'f':
+        temp = (temp - 32) * 5/9
 
     return temp
-
 
 convert()
