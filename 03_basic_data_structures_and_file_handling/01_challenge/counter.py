fef7d8b6e42f8f023fb1a28d05dcb4e5a386f091
Jiro Fujino <jiro.fujino@woven-planet.global>
2024-03-20 22:02:47 +0900

    Submit challenge for grading  (採点用にチャレンジを提出)

--- 03_basic_data_structures_and_file_handling/01_challenge/counter.py
+++ 03_basic_data_structures_and_file_handling/01_challenge/counter.py
@@ -14,7 +14,16 @@ def counter():
     ]
 
     # ここにコードを書いてください
-
+    #各果物の出現回数をカウントしてoccurrencesに格納する
+    for fruit in fruits:
+        if fruit not in occurrences:
+            occurrences[fruit] = 1
+        else:
+            occurrences[fruit] += 1
+    
+    #カウント結果を出力する
+    for fruit, count in occurrences.items():
+        print(f"{fruit}: {count}")
     return occurrences
 
 print(counter())
